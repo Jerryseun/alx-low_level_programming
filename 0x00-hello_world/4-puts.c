5003b40066a2c24d894bd055e1bc32c632fb4872
Jerryseun <adetunjijeremiah1@gmail.com>
2023-06-15 12:13:44 -0700

    print to output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!bin/bash
+#include <stdio.h>
+/*
+ * main - Entry point
+ * prints using puts
+ * Return - 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
