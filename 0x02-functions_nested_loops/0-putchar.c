9a5671f3b967f73128ca81774c112bd20e7c496e
Jerryseun <adetunjijeremiah1@gmail.com>
2023-06-20 13:24:04 -0700

    print with using a file header that contain _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,15 @@
   */
 int main(void)
 {
-	_putchar("M");
-	_putchar("a")
-	_putchar("i");
-	_putchar("n");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 
