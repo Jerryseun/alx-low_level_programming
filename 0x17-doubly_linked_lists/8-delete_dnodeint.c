f4a3fed8274f0fea5d917d9fa4970e20a12bcf8e
Jerryseun <adetunjijeremiah1@gmail.com>
2023-11-11 03:33:12 +0100

    error fixed

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,38 +1,52 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete at index in a DBL
- * @head: the head of the linked list
- * @index: the index
- * Return: 1 on succes and -1 else
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
+ *
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *new, *last, *back;
-	unsigned int n;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	if (*head == NULL)
+	h1 = *head;
+
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		return (-1);
-	}
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (-1);
-	}
-	last = *head;
-	while (last)
-	{
-		if (index == n)
+		if (i == index)
 		{
-			back = last->prev;
-			new = last->next;
-			last->prev = back;
-			last->next = new;
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
 			return (1);
 		}
-		last = last->next;
-		n++;
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
+
 	return (-1);
 }
